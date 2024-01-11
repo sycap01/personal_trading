5d2df6c1774d0f4152b2c63c1f975a72de8df335
sycap <sycap01@gmail.com>
2024-01-11 10:39:45 +0900

    test

---
+++ src/main/java/com/sypark/personal_trading/PersonalTradingApplication.java
@@ -0,0 +1,13 @@
+package com.sypark.personal_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sypark/personal_trading/PersonalTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sypark.personal_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
